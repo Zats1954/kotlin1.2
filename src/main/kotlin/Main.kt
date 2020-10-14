bdfb319a8b0a29bd239a7a340fa78fbd0e3b0a3c
Alexander <alex.zacepin@gmail.com>
2020-10-14 07:15:03 +0200

    divide 100

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ fun main(){
     val number1 = Scanner(System.`in`)
     print("Введите число лайков: ")
     val likes = number1.nextInt()
-    val lastDigit = likes % 10
-    val word = if(lastDigit == 1 && likes != 11){" человеку"} else {     " людям"   }
+    val word = if(likes % 10 == 1 && likes%100 != 11){" человеку"} else {     " людям"   }
     print("Понравилось $likes $word")
 }
